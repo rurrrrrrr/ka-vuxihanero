eb3fb2349b8cf119c83f8e6d0750ea29cb8af154
rurrrrrrr <sshuku241146@shukutoku.ed.jp>
2026-09-09 09:20:42 +0000

    ドパガキ

--- sketch.js
+++ sketch.js
@@ -3,13 +3,20 @@ let gravity;
 let baseline;
 let player;
 let balls = [];
+let specialBalls = [];
 let lastBallTime;
+let lastSpecialBallTime;
+let specialBallInterval;
 let startTime;
 let scoreTime;
+let jumpUnlimitedUntil;
+let cheatInput = "";
+let invincible = false;
+const cheatCode = "nitijixyousai";
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(1000, 400);
     angleMode(DEGREES);
-    gravity = 1;
+    gravity = 10;
     baseline = height - 10;
     initGame();
 }
@@ -26,12 +33,24 @@ function draw() {
     }
 }
 function keyPressed() {
-    if (mode == 0 && key == " ") {
-        initGame();
-        mode = 1;
+    if (mode == 0) {
+        if (key == " ") {
+            initGame();
+            cheatInput = "";
+            mode = 1;
+        } else if (key.length == 1 && /[a-z]/i.test(key)) {
+            cheatInput = (cheatInput + key.toLowerCase()).slice(-cheatCode.length);
+            if (cheatInput == cheatCode) {
+                initGame();
+                invincible = true;
+                mode = 1;
+                cheatInput = "";
+            }
+        }
     } else if (mode == 1 && key == " ") {
         playerJump();
     } else if (mode == 2 && key == " ") {
+        cheatInput = "";
         mode = 0;
     }
 }
@@ -48,10 +67,14 @@ function playGame() {
     updatePlayer();
     addBalls();
     updateBalls();
+    addSpecialBalls();
+    updateSpecialBalls();
     checkHits();
+    checkSpecialBallHits();
     updateScore();
     drawPlayer();
     drawBalls();
+    drawSpecialBalls();
     drawScore();
 }
 function showGameOver() {
@@ -61,6 +84,7 @@ function showGameOver() {
     text("GAME OVER", width / 2, height / 2);
     drawPlayer();
     drawBalls();
+    drawSpecialBalls();
     drawScore();
 }
 function initGame() {
@@ -73,9 +97,14 @@ function initGame() {
         jumpCount: 0,
     };
     balls = [];
+    specialBalls = [];
     lastBallTime = millis();
+    lastSpecialBallTime = millis();
+    specialBallInterval = 10000;
     startTime = millis();
     scoreTime = 0;
+    jumpUnlimitedUntil = 0;
+    invincible = false;
 }
 function drawPlayer() {
     kirby(player.x, player.y, player.radius, player.angle);
@@ -91,9 +120,11 @@ function updatePlayer() {
     }
 }
 function playerJump() {
-    if (player.jumpCount > 0) {
-        player.speedY = -18;
-        player.jumpCount = player.jumpCount - 1;
+    if (millis() < jumpUnlimitedUntil || player.jumpCount > 0) {
+        player.speedY = -50;
+        if (millis() >= jumpUnlimitedUntil) {
+            player.jumpCount = player.jumpCount - 1;
+        }
     }
 }
 function drawBalls() {
@@ -102,14 +133,17 @@ function drawBalls() {
     }
 }
 function addBalls() {
-    if (millis() - lastBallTime > 1500) {
+    let elapsedTime = millis() - startTime;
+    let fastBallPeriod = elapsedTime >= 11000 && (elapsedTime - 11000) % 30000 < 9000;
+    let ballInterval = fastBallPeriod ? 500 : 800;
+    if (millis() - lastBallTime > ballInterval) {
         lastBallTime = millis();
         balls.push({
             x: width + 50,
             y: baseline - 150,
-            radius: 20,
-            speedX: 10,
-            speedY: 0,
+            radius: 30,
+            speedX: 20,
+            speedY: 5,
             angle: 0,
         });
     }
@@ -117,7 +151,7 @@ function addBalls() {
 function updateBalls() {
     for (let i = 0; i < balls.length; i++) {
         balls[i].x = balls[i].x - balls[i].speedX;
-        balls[i].angle = balls[i].angle - 10;
+        balls[i].angle = balls[i].angle - 100;
         balls[i].speedY = balls[i].speedY + gravity;
         balls[i].y = balls[i].y + balls[i].speedY;
         if (balls[i].y >= baseline - balls[i].radius) {
@@ -126,13 +160,55 @@ function updateBalls() {
         }
     }
 }
+function addSpecialBalls() {
+    if (millis() - lastSpecialBallTime > specialBallInterval) {
+        lastSpecialBallTime = millis();
+        specialBallInterval = 30000;
+        specialBalls.push({
+            x: player.x,
+            y: -50,
+            radius: 24,
+            speedY: 5,
+            angle: 0,
+        });
+    }
+}
+function updateSpecialBalls() {
+    for (let i = specialBalls.length - 1; i >= 0; i--) {
+        specialBalls[i].angle = specialBalls[i].angle + 8;
+        specialBalls[i].speedY = specialBalls[i].speedY + gravity;
+        specialBalls[i].y = specialBalls[i].y + specialBalls[i].speedY;
+        if (specialBalls[i].y > height + specialBalls[i].radius) {
+            specialBalls.splice(i, 1);
+        }
+    }
+}
+function drawSpecialBalls() {
+    for (let i = 0; i < specialBalls.length; i++) {
+        specialBall(specialBalls[i].x, specialBalls[i].y, specialBalls[i].radius, specialBalls[i].angle);
+    }
+}
 function checkHits() {
+    if (invincible) {
+        return;
+    }
     for (let i = 0; i < balls.length; i++) {
         if (
             dist(player.x, player.y, balls[i].x, balls[i].y) <
             player.radius + balls[i].radius
         ) {
             mode = 2;
+        }
+    }
+}
+function checkSpecialBallHits() {
+    for (let i = specialBalls.length - 1; i >= 0; i--) {
+        if (
+            dist(player.x, player.y, specialBalls[i].x, specialBalls[i].y) <
+            player.radius + specialBalls[i].radius
+        ) {
+            jumpUnlimitedUntil = millis() + 10000;
+            specialBalls.splice(i, 1);
         }
     }
 }
@@ -144,6 +220,16 @@ function drawScore() {
     textAlign(LEFT);
     textSize(14);
     text("SCORE: " + scoreTime, width - 120, 25);
+    if (millis() < jumpUnlimitedUntil) {
+        textAlign(CENTER);
+        fill("#1565C0");
+        text("JUMP UNLIMITED: " + ceil((jumpUnlimitedUntil - millis()) / 1000), width / 2, 25);
+    }
+    if (invincible) {
+        textAlign(CENTER);
+        fill("#D32F2F");
+        text("無敵モード中！", width / 2, 45);
+    }
 }
 function kirby(x, y, radius, angle) {
     push();
@@ -180,5 +266,20 @@ function monsterBall(x, y, radius, angle) {
     line(-radius + 3, 0, radius - 3, 0);
     fill("white");
     circle(0, 0, radius / 2);
+    pop();
+}
+function specialBall(x, y, radius, angle) {
+    push();
+    translate(x, y);
+    rotate(angle);
+    stroke("#1565C0");
+    strokeWeight(3);
+    fill("#81D4FA");
+    circle(0, 0, radius * 2);
+    fill("white");
+    circle(0, 0, radius);
+    strokeWeight(2);
+    line(-radius / 2, 0, radius / 2, 0);
+    line(0, -radius / 2, 0, radius / 2);
     pop();
 }
